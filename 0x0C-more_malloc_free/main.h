e60be7084626258a441f46040a579b6749e6a642
adidazbot <ayenicharlesadidaz@gmail.com>
2023-06-15 17:14:44 +0000

    malloc check

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <string.h>
 #include <stdlib.h>
+int main(void);
 char *_initialize_array(char *ar, int lar);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
@@ -10,5 +11,4 @@ int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 int _checknum(char *argv[], int n);
-int main(int argc, char *argv[]);
 #endif
