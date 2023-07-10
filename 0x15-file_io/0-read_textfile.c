c1230c06df3d4fba3cab6e1472f47f2c6a755ee6
adidazbot <ayenicharlesadidaz@gmail.com>
2023-07-10 18:47:49 +0000

    requestcat

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t ffd;
+	ssize_t ww;
+	ssize_t tt;
+
+	ffd = open(filename, O_RDONLY);
+	if (ffd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	tt = read(ffd, buff, letters);
+	ww = write(STDOUT_FILENO, buff, tt);
+
+	free(buff);
+	close(ffd);
+	return (ww);
+}
